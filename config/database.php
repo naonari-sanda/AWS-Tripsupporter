7a83c8bcbbcf26492fa5f644aefd9b86a8935459
Nonari Sanda <mkg_sandy@icloud.com>
2021-02-19 16:53:25 +0900

    config.ymlにmysqlの設定を記述しました

--- config/database.php
+++ config/database.php
@@ -67,7 +67,7 @@ return [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
-            'database' => 'circle_test',
+            'database' => 'database',
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8mb4',
