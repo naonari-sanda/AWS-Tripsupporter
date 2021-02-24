5f9f733ab6a678ef2fd4b87d341fde5af859c926
Nonari Sanda <mkg_sandy@icloud.com>
2021-02-24 10:35:09 +0900

    cssの読み込みをasset()からmix()に変更しました

--- resources/views/includes/common/head.blade.php
+++ resources/views/includes/common/head.blade.php
@@ -23,6 +23,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js" integrity="sha512-k2GFCTbp9rQU412BStrcD/rlwv1PYec9SNrkbQlo6RZCf75l6KcC3UwDY8H5n5hl4v77IDtIPwOk9Dqjs/mMBQ==" crossorigin="anonymous"></script>
 
 <!-- Styles -->
-<link href="{{ asset('css/fontawesome-stars.css') }}" rel="stylesheet">
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-<link href="{{ asset('css/toastr.min.css') }}" rel="stylesheet">
+
+<link href="{{ mix('css/app.css') }}" rel="stylesheet">
